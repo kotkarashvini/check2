765c1f89b39f737c4a29032ae3c811b44ef71a23
gitanjali <maligitanjali123@gmail.com>
2022-04-04 12:53:26 +0530

    2 files added sum and diff

---
+++ check2/diff.java
@@ -0,0 +1,15 @@
+public class diff {
+    public static void main(String[] args) {
+        System.out.println("2 file");
+<<<<<<< HEAD
+        System.out.println("cogtgggmmit");
+        System.out.println("in 22222222 added fro, visula stuuasbajs");
+        System.out.println("in 22222222 added fro, visula stuuasbajsdfdsfsdfs");
+        System.out.println("in 444444 added fro, visula stuuasbajsdfdsfsdfs");
+=======
+        System.out.println("commit");
+        System.out.println("in git hub changes r done in diff.java");
+        
+>>>>>>> ee5441be4f943f3eca873da3ca6cb271d2621b23
+    }
+}
